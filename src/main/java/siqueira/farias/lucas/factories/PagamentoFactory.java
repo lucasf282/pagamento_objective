5f221e4ee7791ef93b2dbc46fbcdc69a1e8b58d8
Lucas Farias <lucasfsiqueira282@gmail.com>
2025-01-06 00:05:03 -0300

    refactor: aplicar switch expression nas factories

--- src/main/java/siqueira/farias/lucas/factories/PagamentoFactory.java
+++ src/main/java/siqueira/farias/lucas/factories/PagamentoFactory.java
@@ -2,19 +2,16 @@ package siqueira.farias.lucas.factories;
 
 import siqueira.farias.lucas.enums.FormaPagamentoEnum;
 import siqueira.farias.lucas.strategies.pagamentos.*;
+import siqueira.farias.lucas.utils.MessageUtil;
 
 public class PagamentoFactory {
 
     public static PagamentoStrategy getPagamentoStrategy(FormaPagamentoEnum formaPagamento) {
-        switch (formaPagamento) {
-            case CARTAO_CREDITO:
-                return new PagamentoCartaoCredito();
-            case CARTAO_DEBITO:
-                return new PagamentoCartaoDebito();
-            case PIX:
-                return new PagamentoPix();
-            default:
-                throw new IllegalArgumentException("Forma de pagamento não suportada: " + formaPagamento);
-        }
+        return switch (formaPagamento) {
+            case CARTAO_CREDITO -> new PagamentoCartaoCredito();
+            case CARTAO_DEBITO -> new PagamentoCartaoDebito();
+            case PIX -> new PagamentoPix();
+            default -> throw new IllegalArgumentException(MessageUtil.getMessage("formaPagamento.notFound"));
+        };
     }
 }
