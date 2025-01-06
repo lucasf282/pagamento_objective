6e70708bc6512bd612315d0b7c6301c48b0fae10
Lucas Farias <lucasfsiqueira282@gmail.com>
2025-01-06 00:11:32 -0300

    test: adicionar testes de integração para cadastro de contas.

--- src/test/java/siqueira/farias/lucas/ContaResourceTest.java
+++ src/test/java/siqueira/farias/lucas/ContaResourceTest.java
@@ -2,19 +2,28 @@ package siqueira.farias.lucas;
 
 import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Test;
+import siqueira.farias.lucas.dtos.ContaDTO;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.CoreMatchers.is;
 
 @QuarkusTest
 class ContaResourceTest {
     @Test
-    void testHelloEndpoint() {
+    void testCadastro() {
+        ContaDTO conta = new ContaDTO();
+        conta.setNumeroConta(123L);
+        conta.setSaldo(100.0f);
+
         given()
-          .when().get("/hello")
-          .then()
-             .statusCode(200)
-             .body(is("Hello from Quarkus REST"));
+              .contentType("application/json")
+              .body(conta)
+              .when().post("/conta")
+              .then()
+              .statusCode(200)
+              .body(containsString("\"saldo\":100.0"))
+              .body(containsString("\"numero_conta\":123"));
     }
 
 }
